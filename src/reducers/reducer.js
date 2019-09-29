6a4f73870e43099caeed926a0288a9e69f5f9a3d
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-29 14:50:43 -0400

    configures reducer to handle different action types

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,8 +1,28 @@
+import {FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_FAIL} from "../actions/actions";
+
 const initialState = {};
 
 function reducer(state = initialState, action) {
     switch (action.type) {
-        case '':
+        case FETCH_DATA_START:
+            return {
+                ...state,
+                isFetching: true,
+                error: ""
+            };
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                characters: action.payload
+                isFetching: false,
+                error: "none"
+            };
+        case FETCH_DATA_FAIL:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
         default:
             return state;
     }
