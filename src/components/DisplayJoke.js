c57f84b60464caf5dafa24e872d73706f6daf990
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-29 16:15:55 -0400

    makes punchlines initially invisible w/ a visibility toggle button

--- src/components/DisplayJoke.js
+++ src/components/DisplayJoke.js
@@ -2,10 +2,16 @@ import React from 'react';
 import '../App.css';
 
 const DisplayJoke = props => {
+
+    const toggleVisibility = (elementID) => {
+        document.getElementById(elementID).classList.remove('invisibleElement')
+    };
+
     return (
         <div className="jokeBox">
             <p>{props.joke.setup}</p>
-            <p>{props.joke.punchline}</p>
+            <button onClick={() => toggleVisibility(props.joke.id)}>Tell me!</button>
+            <p className="invisibleElement" id={props.joke.id}>{props.joke.punchline}</p>
         </div>
     )
 };
